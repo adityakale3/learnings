3b098b2225a2b724502ffbc60aca4c7e0ccb296d
Aditya Kale <adityakale3@gmail.com>
2020-10-06 12:49:47 +0530

    Objects

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-let reverse = (args) => {
-  return args.split(" ").reverse().join(" ");
+let arr = [11, 12, 13, 4, 5, 6, 7, 14, 15, 16, 17, 19, 20, 1, 2, 3, 8, 9, 10];
+
+let newArr = arr.sort((a, b) => a - b);
+
+let missingError = (newArr) => {
+  for (let i = 0; i < newArr.length; i++) {
+    if (i + 1 !== newArr[i]) {
+      return i + 1;
+    }
+  }
 };
 
-console.log(reverse("Moon to Sun And Galaxy"));
+console.log(missingError(newArr));
